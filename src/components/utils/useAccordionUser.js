21a0b454ee9f9127e5617bbd135319b5cab75f56
Suraj Yadav <suraj.yadav@tubelightcommunications.com>
2023-08-16 17:29:17 +0530

    removed useCallback hook

--- src/components/utils/useAccordionUser.js
+++ src/components/utils/useAccordionUser.js
@@ -1,14 +1,14 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 
 const useUserAccordion = (initialData) => {
   const [editedUser, setEditedUser] = useState({ ...initialData }); // shallow copy
 
-  const handleInputChange = useCallback((field, value) => {
+  const handleInputChange = (field, value) => {
     setEditedUser((prevUser) => ({
       ...prevUser,
       [field]: value,
     }));
-  }, []);
+  };
 
   return {
     editedUser,
